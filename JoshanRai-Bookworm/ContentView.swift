30ee541788823e4155be116672243a78943da375
Joshan Rai <pradheon@gmail.com>
2022-03-27 20:58:33 -0700

    Initial Commit

---
+++ JoshanRai-Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JoshanRai-Bookworm
+//
+//  Created by Joshan Rai on 3/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JoshanRai-Bookworm/JoshanRai_BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_BookwormApp.swift
+//  JoshanRai-Bookworm
+//
+//  Created by Joshan Rai on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
